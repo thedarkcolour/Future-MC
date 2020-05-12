28ac27571a6e575307132202e2bf0e651560b5fd
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2020-05-11 20:24:21 -0700

    Begin rewriting ASM in Kotlin with a system inspired by Quark

---
+++ src/main/java/thedarkcolour/futuremc/asm/ASMUtil.kt
@@ -0,0 +1,25 @@
+// I decided to take some inspiration from Quark's ASM.
+// Hopefully this simplifies things and I can
+// put everything in the same spot.
+
+package thedarkcolour.futuremc.asm
+
+import org.objectweb.asm.tree.AbstractInsnNode
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.MethodInsnNode
+import org.objectweb.asm.tree.MethodNode
+
+typealias MethodAction = (MethodNode) -> Boolean
+typealias NodeFilter = (AbstractInsnNode) -> Boolean
+typealias NodeAction = (MethodNode, AbstractInsnNode) -> Boolean
+typealias TransformerAction = (ClassNode) -> Boolean
+
+class MethodSignature(private val mappedName: String, private val srgName: String, private val desc: String) {
+    fun matches(name: String, desc: String): Boolean {
+        return (mappedName == name || srgName == name) && this.desc == desc
+    }
+
+    fun matches(node: MethodNode) = matches(node.name, node.desc)
+
+    fun matches(node: MethodInsnNode) = matches(node.name, node.desc)
+}
