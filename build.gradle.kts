e89e740e62e5fb5b0db731f1b831991fd96a955f
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2021-10-09 20:24:12 -0700

    Version 0.2.7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(8))
 idea.module.inheritOutputDirs = true
 
-version = "0.2.7"
+version = "0.2.7.1"
 group = "thedarkcolour.futuremc"
 
 minecraft {
