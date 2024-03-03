260a1b3bbed5e807a02d29622e9755aef06d9f31
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2024-03-02 21:42:26 -0800

    Add graphics for Game Mode Switcher and improve accuracy to Vanilla

--- src/main/java/thedarkcolour/futuremc/client/ClientEvents.kt
+++ src/main/java/thedarkcolour/futuremc/client/ClientEvents.kt
@@ -3,19 +3,28 @@ package thedarkcolour.futuremc.client
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.GuiMerchant
 import net.minecraft.client.model.ModelBiped
+import net.minecraft.client.renderer.GlStateManager
+import net.minecraft.client.renderer.RenderHelper
 import net.minecraft.client.renderer.block.model.ModelResourceLocation
+import net.minecraft.client.resources.I18n
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.init.Blocks
+import net.minecraft.init.Items
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumHand
+import net.minecraft.util.ResourceLocation
 import net.minecraft.world.GameType
 import net.minecraftforge.client.event.GuiOpenEvent
 import net.minecraftforge.client.event.ModelBakeEvent
 import net.minecraftforge.client.event.ModelRegistryEvent
+import net.minecraftforge.client.event.RenderGameOverlayEvent
 import net.minecraftforge.client.model.ModelLoader
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent
 import org.lwjgl.input.Keyboard
+import org.lwjgl.input.Mouse
+import thedarkcolour.futuremc.FutureMC
 import thedarkcolour.futuremc.client.gui.GuiVillager
 import thedarkcolour.futuremc.client.render.TridentBakedModel
 import thedarkcolour.futuremc.config.FConfig
@@ -25,9 +34,15 @@ import thedarkcolour.futuremc.network.NetworkHandler
 
 object ClientEvents {
     var prevGameMode = GameType.CREATIVE
-    var selected = GameType.SURVIVAL
+    var selected = GameType.NOT_SET
     val models = ArrayList<Triple<Item, Int, String>>()
-    var showGameModeSwitcher = false
+    private var showGameModeSwitcher = false
+    private var oldDebugSetting = false
+    @JvmStatic
+    private val gameModeSwitcherTexture = ResourceLocation(FutureMC.ID, "textures/gui/gamemode_switcher.png")
+    @JvmStatic
+    private val gameTypeItems = arrayOf(ItemStack(Blocks.GRASS), ItemStack(Items.IRON_SWORD), ItemStack(Items.MAP), ItemStack(Items.ENDER_EYE))
+    private val gameTypes = arrayOf(GameType.CREATIVE, GameType.SURVIVAL, GameType.ADVENTURE, GameType.SPECTATOR)
 
     @SubscribeEvent
     fun onGuiOpen(event: GuiOpenEvent) {
@@ -64,14 +79,26 @@ object ClientEvents {
     fun onKeyInput(event: KeyInputEvent) {
         if (!FConfig.netherUpdate.gameModeSwitcher) {
             showGameModeSwitcher = false
+            return
         }
-        // todo escape will cancel the gamemode switch
         if (Keyboard.isKeyDown(Keyboard.KEY_F3)) {
             if (Keyboard.getEventKey() == Keyboard.KEY_F4 && Keyboard.getEventKeyState()) {
                 // just selected
                 if (!showGameModeSwitcher) {
-                    // todo grab mouse
-                    selected = prevGameMode
+                    val currentGameMode = getGameMode()
+                    if (selected == GameType.NOT_SET) {
+                        if (currentGameMode == GameType.CREATIVE) {
+                            selected = GameType.SURVIVAL
+                        } else {
+                            selected = GameType.CREATIVE
+                        }
+                    }
+                    if (currentGameMode != prevGameMode) {
+                        selected = prevGameMode
+                    }
+                    // Release cursor
+                    Minecraft.getMinecraft().inGameHasFocus = false
+                    Minecraft.getMinecraft().mouseHelper.ungrabMouseCursor()
                 } else {
                     // cycle game mode
                     selected = when (selected) {
@@ -82,20 +109,74 @@ object ClientEvents {
                     }
                 }
                 showGameModeSwitcher = true
+                // Save setting to avoid opening F3 menu when selected
+                oldDebugSetting = Minecraft.getMinecraft().gameSettings.showDebugInfo
             }
         } else if (Keyboard.getEventKey() == Keyboard.KEY_F3) {
-            // release from menu
-            // todo release mouse
-            showGameModeSwitcher = false
+            if (showGameModeSwitcher) {
+                Minecraft.getMinecraft().gameSettings.showDebugInfo = oldDebugSetting
+                Minecraft.getMinecraft().inGameHasFocus = true
+                Minecraft.getMinecraft().mouseHelper.grabMouseCursor()
 
-            val player = Minecraft.getMinecraft().player
+                val player = Minecraft.getMinecraft().player
 
-            if (player != null && player.permissionLevel >= 2) {
-                if (Minecraft.getMinecraft().connection?.getPlayerInfo(Minecraft.getMinecraft().player.gameProfile.id)?.gameType != selected) {
-                    NetworkHandler.sendGameModeSwitch(selected)
+                if (player != null && player.permissionLevel >= 2) {
+                    if (getGameMode() != selected) {
+                        NetworkHandler.sendGameModeSwitch(selected)
+                    }
                 }
             }
+            // release from menu
+            showGameModeSwitcher = false
         }
+    }
+
+    @SubscribeEvent
+    fun renderOverlay(event: RenderGameOverlayEvent.Post) {
+        if (showGameModeSwitcher && event.type == RenderGameOverlayEvent.ElementType.ALL) {
+            GlStateManager.enableBlend()
+            GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO)
+            RenderHelper.enableGUIStandardItemLighting()
+            val mc = Minecraft.getMinecraft()
+            mc.textureManager.bindTexture(gameModeSwitcherTexture)
+
+            val resolutionWidth = event.resolution.scaledWidth
+            val resolutionHeight = event.resolution.scaledHeight
+            val mouseX = Mouse.getX() * resolutionWidth / mc.displayWidth
+            val mouseY = resolutionHeight - Mouse.getY() * resolutionHeight / mc.displayHeight - 1
+            val x = resolutionWidth / 2
+            val y = resolutionHeight / 2 - 31
+            mc.ingameGUI.drawTexturedModalRect(x - 62, y - 27, 0, 0, 125, 75)
+
+            for (i in 0..3) {
+                // render
+                val x1 = x - 119 / 2 + i * 31
+
+                // hover select
+                if (x1 <= mouseX && mouseX <= x1 + 26 && y <= mouseY && mouseY <= y + 26) {
+                    selected = gameTypes[i]
+                }
+
+                mc.ingameGUI.drawTexturedModalRect(x1, y, 0, 75, 26, 26)
+                if (selected == gameTypes[i]) {
+                    // render select
+                    mc.ingameGUI.drawTexturedModalRect(x1, y, 26, 75, 26, 26)
+                }
+            }
+            for (i in 0..3) {
+                val x1 = x - 119 / 2 + i * 31
+                mc.renderItem.renderItemAndEffectIntoGUI(gameTypeItems[i], x1 + 5, y + 5)
+            }
+
+            mc.ingameGUI.drawCenteredString(mc.fontRenderer, I18n.format("gameMode." + selected.getName()), x, y - 20, 0xffffff)
+            mc.ingameGUI.drawCenteredString(mc.fontRenderer, "[ F4 ]", x, y + 5 + 31, 5636095)
+            GlStateManager.disableBlend()
+            RenderHelper.disableStandardItemLighting()
+        }
+    }
+
+    private fun getGameMode(): GameType {
+        return Minecraft.getMinecraft().connection?.getPlayerInfo(Minecraft.getMinecraft().player.gameProfile.id)?.gameType ?: GameType.NOT_SET
     }
 
     /**
